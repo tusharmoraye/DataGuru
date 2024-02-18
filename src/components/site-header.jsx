023b9229c61b31f8e8ebddded3c79e9ae284eee9
Tushar Moraye <tusharmoraye@gmail.com>
2024-02-18 21:46:14 +0530

    add base url for github pages support

--- src/components/site-header.jsx
+++ src/components/site-header.jsx
@@ -4,7 +4,11 @@ export function SiteHeader() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 max-w-screen-2xl items-center">
-        <img className="w-8 h-8 mr-4" src="/favicon.jpg" alt="favicon" />
+        <img
+          className="w-8 h-8 mr-4"
+          src={`${import.meta.env.BASE_URL}favicon.jpg`}
+          alt="favicon"
+        />
         <h2 className="text-2xl">Data Guru</h2>
         <div className="ml-auto">
           <ModeToggle />
--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
+const ASSET_URL = process.env.ASSET_URL || "";
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: `${ASSET_URL}/`,
   plugins: [react()],
   resolve: {
     alias: {
